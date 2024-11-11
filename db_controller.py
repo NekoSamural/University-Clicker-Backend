201560116a9f931d5704fd9519d212c7f61dc77c
NekoSamurai <44811270+NekoSamural@users.noreply.github.com>
2024-11-11 05:58:28 +0300

    Добавил функцию обновления токена в БД.

--- db_controller.py
+++ db_controller.py
@@ -2,26 +2,41 @@ import sqlite3
 from configs import DATA_BASE_CONFIG
 from models.data_models import PlayerData, PlayerPassword
 
-def execute_query(query: str) -> list[any]:
+def get_player_data_by_login(login:str) -> PlayerPassword | None:
     connection = sqlite3.connect(DATA_BASE_CONFIG.path)
     cursor = connection.cursor()
-    
-    cursor.execute(query)
+
+    cursor.execute(
+        f"SELECT id, password FROM Players WHERE login = '{login}' LIMIT 1"
+    )
     result = cursor.fetchall()
+
+    connection.close()
+
+    if len(result) == 0:
+        return None
+    
+    id = result[0][0]
+    password = result[0][1]
+
+    return PlayerPassword(id, password)
+
+def update_player_token(id:int, token:str) -> bool:
+    connection = sqlite3.connect(DATA_BASE_CONFIG.path)
+    cursor = connection.cursor()
+
+    cursor.execute(
+        f"SELECT count(id)>0 FROM Players WHERE id = '{id}'"
+    )
+    result = cursor.fetchall()[0][0]
+    if result == False:
+        connection.close()
+        return False
+    
+    cursor.execute(
+        f"UPDATE Players SET token = '{token}' WHERE id = '{id}'"
+    )
     connection.commit()
     connection.close()
 
-    return result
-
-def get_player_data_by_login(login:str) -> PlayerPassword | None:
-    data = execute_query(
-        f"SELECT id, password FROM Players WHERE login = '{login}' LIMIT 1"
-    )
-
-    if len(data) == 0:
-        return None
-    
-    id = data[0][0]
-    password = data[0][1]
-
-    return PlayerPassword(id, password)
+    return True
